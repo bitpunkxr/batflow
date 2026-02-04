26b9fb127f3d110a47f8bc284240496a473c2304
bitpunkxr <tony69elvis@gmail.com>
2026-02-04 03:06:53 +0100

    Updated apps and git commnds for VS code. Place in startup folder in windows for executiion after boot up

--- autorun.bat
+++ autorun.bat
@@ -1,6 +1,16 @@
+title Autorun Script
+@echo off
+
 start "" "C:\Users\irhayeelvis\AppData\Local\Programs\Microsoft VS Code"
-@REM start to vs code for example
+@REM start to vs code
 start "" "C:\Program Files\Google\Chrome\Application\chrome.exe"
-@REM start to google chrome for example
+@REM start to google chrome
 start "" "C:\Program Files\VideoLAN\VLC\vlc.exe"
-@REM start to vlc media player for example
+@REM start to vlc media player
+
+git init
+code .
+@REM initialize a git repository and open VS Code in the current directory
+
+closed
+@REM close the command prompt window after executing the commands
