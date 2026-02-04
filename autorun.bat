0c193ca89313e5876d8dd6ccd3efc6bc79402bae
bitpunkxr <tony69elvis@gmail.com>
2026-02-04 03:03:20 +0100

    Start UP programs setUp

---
+++ autorun.bat
@@ -0,0 +1,6 @@
+start "" "C:\Users\irhayeelvis\AppData\Local\Programs\Microsoft VS Code"
+@REM start to vs code for example
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe"
+@REM start to google chrome for example
+start "" "C:\Program Files\VideoLAN\VLC\vlc.exe"
+@REM start to vlc media player for example
